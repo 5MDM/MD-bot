d09e81b473ea646d273c87b8eef9882775c3e9f8
MD <110855261+5MDM@users.noreply.github.com>
2022-12-22 22:08:36 +0000

    Initial commit

---
+++ src/commands.js
@@ -0,0 +1,91 @@
+import {SlashCommandBuilder} from "@discordjs/builders";
+import {sendMsg, sendBasicMsg} from "./framework.js";
+
+const commands = [];
+const commandMain = [];
+const commandExec = {};
+
+function push([name, a, b]) {
+  commandMain.push(a);
+  commands.push([a, b]);
+  commandExec[name] = b;
+}
+
+push([
+  "say",
+  new SlashCommandBuilder()
+  .setName("say")
+  .setDescription("Make the bot say something")
+  .addStringOption(opt => 
+    opt.setName("text")
+    .setDescription("What the bot will say")
+    .setRequired(true)
+  ),
+  async function(e) {
+    const txt = e.options.getString("text");
+    await e.reply({content: "Message sent", ephemeral: true});
+    sendBasicMsg(e, txt);
+  },
+]);
+push([
+  "date",
+  new SlashCommandBuilder()
+  .setName("date")
+  .setDescription("Show today's real date"),
+  async function(e) {
+    const date = (() => {
+      const year=new Date().getFullYear();
+      function between(start, end){start=new Date(start);
+      end=new Date(end);const day=1000*60*60*24;
+      const ms=end.getTime()-start.getTime();
+      return Math.round(ms/day)}const october=`10/1/${ year }`;
+      var start=new Date();var startYear=start.getFullYear();
+      if(start.getMonth()<9){startYear++}start=
+      `${start.getMonth()+1}/${start.getDate()+1}/${ startYear }`;
+      const endDate=between(october,start).toString();
+      var finalDate=endDate;switch(Number(endDate[endDate.length-1]))
+      {case 1:finalDate+="st";break;case 2:finalDate+="nd";break;
+      case 3:finalDate+="rd";break;default:finalDate+="th"}
+      return finalDate;
+    })();
+    await e.reply("Today is October " + date);
+  },
+]);
+push([
+  "js",
+  new SlashCommandBuilder()
+  .setName("js")
+  .setDescription("Owner only")
+  .addStringOption(opt => 
+    opt.setName("code")
+    .setDescription("What the bot will run")
+    .setRequired(true)
+  ),
+  async function(e) {
+    const code = e.options.getString("code");
+    if(e.user.id != 991058126339977247) {
+      await e.reply({
+        content: `Happy birthday <@${e.user.id}>`,
+        ephemeral: true,
+      });
+    } else {
+      eval(code);
+      await e.reply({
+        content: "Code succesfully ran",
+        ephemeral: true,
+      });
+    }
+  },
+])
+
+push([
+  "button",
+  new SlashCommandBuilder()
+  .setName("button")
+  .setDescription("DON'T CLICK"),
+  async function(e) {
+    
+  },
+]);
+
+export {commands, commandMain, commandExec};
---
+++ src/framework.js
@@ -0,0 +1,66 @@
+import {
+  Client, GatewayIntentBits, Events,
+  SlashCommandBuilder,
+} from "discord.js";
+
+import {REST} from "@discordjs/rest";
+import {Routes} from "discord-api-types/v9";
+
+var client = null;
+var rest = null;
+
+export function newServer(v = "9", token) {
+  rest = new REST({version: v}).setToken(token);
+  return rest;
+}
+
+export function newClient(token = null) {
+  const e = 
+  new Client({intents: [GatewayIntentBits.Guilds]});
+  client = e;
+  if(token !== null) e.login(token);
+  return e;
+}
+
+export const events = {
+  ready(e) {client.once(Events.ClientReady, e)},
+};
+
+export async function putCommands(arr = []) {
+  try {
+    return await rest.put(
+      Routes.applicationCommands(process.env.client_id),
+      {body: arr},
+    );
+  } catch(err) {
+    console.error("Error: " + err);
+  }
+}
+
+export async function listenForCommands(f) {
+  return client.on(Events.InteractionCreate, f);
+}
+
+export function getGuild(e) {return e.guildId}
+
+export function getChannel(e) {return e.channelId}
+
+export function getUserId(e) {return e.user.id}
+
+export function getUsername(e) {return e.user.name}
+
+export function isUserBot(e) {return e.bot}
+
+export function sendMsg({guild, channel, msg}) {
+  return client.guilds.cache
+  .get(guild).channels.cache
+  .get(channel).send(msg);
+}
+
+export function sendBasicMsg(e, msg) {
+  return sendMsg({
+    guild: e.guildId,
+    channel: e.channelId,
+    msg,
+  });
+}
